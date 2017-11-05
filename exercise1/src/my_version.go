d5c58db4871435e31a8c30d4b7eb236a3ef3536d
Fanis <fanis.korlos@upstreamsystems.com>
2017-11-05 21:52:25 +0200

    added Bonus version of code

--- exercise1/src/my_version.go
+++ exercise1/src/my_version.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+)
 
 type Version struct {
 	Major, Minor, Patch uint //Choosing uint to avoid erroneous usage of negative value
@@ -11,13 +14,16 @@ func (v Version) VersionApi() {
 }
 
 func main() {
-	//Insert the hard-coded version of the code
-	My_version := Version{
-		Major: 2,
-		Minor: 6,
-		Patch: 5,
-	}
 
-	///Invoke the method VersionApi() to print the hard-coded version
-	My_version.VersionApi()
+	//Declare a pointer to the Version struct to receive the command-line inserted versions
+	valuePtr := &Version{}
+
+	flag.UintVar(&valuePtr.Major, "major", 0, "Major Version of the Code")
+	flag.UintVar(&valuePtr.Minor, "minor", 0, "Minor Version of the Code")
+	flag.UintVar(&valuePtr.Patch, "patch", 0, "Patch Version of the Code")
+
+	flag.Parse()
+
+	//Invoke the method VersionAPi() to print the command-line inserted versions
+	valuePtr.VersionApi() // the same as (*valuePtr).VersionApi()
 }
