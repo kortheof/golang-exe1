f9f67487367fc077a19bb76033da146beb18faec
Fanis <fanis.korlos@upstreamsystems.com>
2017-11-05 20:16:34 +0200

    First version of the code

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# golang-exe1
+# golang-exercises
 Golang Exercises Delivery Repository
+
+Use the following command to clone this Repository to a local directory
+
+$ git clone https://github.com/kortheof/golang-exercises.git
+
+$ cd ./golang-exercises
+
+In order to view the source code of the first exercise:
+
+$ cd ./exercise1/src/
+
+To execute the code:
+
+$ go run my_version.go
---
+++ exercise1/src/my_version.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+type Version struct {
+	Major, Minor, Patch uint16 //Choosing uint to avoid erroneous usage of negative value
+}
+
+func (v Version) VersionApi() {
+	fmt.Printf("{\"version\": \"%d.%d.%d\"}\n", v.Major, v.Minor, v.Patch)
+}
+
+func main() {
+	//Insert the hard-coded version of the code
+	My_version := Version{
+		Major: 2,
+		Minor: 6,
+		Patch: 5,
+	}
+
+	//Call the method VersionApi() to print the hard-coded version
+	My_version.VersionApi()
+}
